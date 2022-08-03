c19c3baa797d2bced5144966aa883cd9c3390dba
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-08-03 16:25:16 -0700

    first test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,43 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('book routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#GET /books should return a list of books', async () => {
+    const resp = await request(app).get('/books');
+    console.log(resp.body);
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual([
+      {
+        id: '1',
+        title: 'The Name of the Wind',
+        released: 2007
+      },
+      {
+        id: '2',
+        title: 'The Wise Mans Fear',
+        released: 2011
+      },
+      {
+        id: '3',
+        title: 'American Gods',
+        released: 2011
+      },
+      {
+        id: '4',
+        title: 'Anansi Boys',
+        released: 2005
+      },
+      {
+        id: '5',
+        title: 'Good Omens',
+        released: 2006
+      },
+    ]);
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,73 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS books CASCADE;
+DROP TABLE IF EXISTS authors CASCADE;
+DROP TABLE IF EXISTS author_and_book CASCADE;
+
+CREATE TABLE books (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR NOT NULL,
+  released INT NOT NULL
+);
+
+CREATE TABLE authors (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  dob DATE,
+  pob VARCHAR
+);
+
+CREATE TABLE author_and_book (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  author_id INT NOT NULL,
+  book_id INT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES authors(id),
+  FOREIGN KEY (book_id) REFERENCES books(id)
+);
+
+INSERT INTO books (title, released) VALUES
+(
+  'The Name of the Wind',
+  2007
+),
+(
+  'The Wise Mans Fear',
+  2011
+),
+(
+  'American Gods',
+  2011
+),
+(
+  'Anansi Boys',
+  2005
+),
+(
+  'Good Omens',
+  2006
+);
+
+INSERT INTO authors (name, dob, pob) VALUES
+(
+  'Patrick Rothfuss',
+  '1973-06-06',
+  'Madison, Wisconsin'
+),
+(
+  'Neil Gaiman',
+  '1960-11-10',
+  'Portchester, Hampshire, England'
+),
+(
+  'Terry Pratchett',
+  '1948-04-28',
+  'Buckinghamshire, England'
+);
+
+INSERT INTO author_and_book (book_id, author_id) VALUES
+  (1, 1),
+  (2, 1),
+  (3, 2),
+  (4, 2),
+  (5, 2),
+  (5, 3);
